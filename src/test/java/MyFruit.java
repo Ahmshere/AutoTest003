1d1bebe9059c64dbfebeae92060ac7dd2bfdfaf9
Ahmshere <asus555post@gmail.com>
2024-01-25 14:21:26 +0200

    msg23dtb

--- src/test/java/MyFruit.java
+++ src/test/java/MyFruit.java
@@ -6,7 +6,7 @@ public enum MyFruit {
         MyFruit myFruit = MyFruit.APPLE;
         System.out.println(myFruit.name());  // Выведет "app"
     }
-
+// дел
     public double sphereSurfaceArea(double r){
         return 4* Math.PI*(r*r);
     }
